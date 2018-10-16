0d0c9197c243fd12f5798bc79fc8d9476c8efa83
Shih Wei <loneswei@gmail.com>
2018-10-16 09:51:32 +0800

    Initial Push
    +SteamVR Plugin
    +Basic Video Script

---
+++ MnM-CoinProject/Assets/Scripts/Video.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+using System.Collections.Generic;
+
+public class Video : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    private int textureIndex = -1;
+    private List<string> videoUrls;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoUrls = new List<string>();
+        LoadVideos();
+        CheckDimensions(videoUrls[0]);
+	}
+	
+    void LoadVideos()
+    {
+        DirectoryInfo directory = new DirectoryInfo(@"C:\MnM Videos\");
+
+        foreach(var file in directory.GetFiles("*.mp4", SearchOption.AllDirectories))
+        {
+            videoUrls.Add(directory.FullName + file.Name);
+        }
+        videoUrls.TrimExcess();
+    }
+
+    void CheckDimensions(string url)
+    {
+        GameObject tempVideo = new GameObject();
+        VideoPlayer tempvideoPlayer = tempVideo.AddComponent<VideoPlayer>();
+        tempvideoPlayer.renderMode = VideoRenderMode.RenderTexture;
+        tempvideoPlayer.targetTexture = new RenderTexture(1, 1, 0);
+        tempvideoPlayer.source = VideoSource.Url;
+        tempvideoPlayer.url = url;
+        tempvideoPlayer.prepareCompleted += (VideoPlayer source) =>
+        {
+            PlayVideo(url, source.texture.width, source.texture.height);
+            Destroy(tempVideo);
+        };
+        tempvideoPlayer.Prepare();
+    }
+
+    void PlayVideo(string url, int width, int height)
+    {
+        videoPlayer.url = url;
+        RenderTexture texture = new RenderTexture(width, height, 24);
+        videoPlayer.targetTexture = texture;
+        RenderSettings.skybox.SetTexture("_MainTex", texture);
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Keypad1))
+        {
+            textureIndex = 0;
+            CheckDimensions(videoUrls[textureIndex]);
+        }
+        else if (Input.GetKeyUp(KeyCode.Keypad2))
+        {
+            textureIndex = 1;
+            CheckDimensions(videoUrls[textureIndex]);
+        }
+    }
+}
