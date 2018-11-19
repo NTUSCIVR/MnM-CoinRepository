846e4fcd7f9ff222fb8aed94144e85fad8bf60da
Shih Wei <loneswei@gmail.com>
2018-11-19 12:54:21 +0800

    Documentation + minor adjustment
    + Fixed csv file duplication bug
    + Added volume control keys

--- MnM-CoinProject/Assets/Scripts/DataCollector.cs
+++ MnM-CoinProject/Assets/Scripts/DataCollector.cs
@@ -1,66 +1,220 @@
-﻿using System.IO;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+﻿//--------------------------------------------------------------------------------
+/*
+ * This Script is used for collecing Data generated/User Input
+ * 
+ * Used in Start Scene, attached to Empty GameObject "DataCollector"
+ * Require 3 InputField variables : User ID, Gender(M / m / F / f), Video ID(1 / 2 / 3 / 4)
+ * which can be found under Canvas GameObject in Hierarchy.
+ * Among the 3 InputFields, only User ID's GameObject need to be active.
+ */
+//--------------------------------------------------------------------------------
+
+using System.IO;    // For Directory, File, StreamWriter
+using UnityEngine;  // Default Unity Script (MonoBehaviour, Application, Tooltip, SerializeField, HideInInspector, DontDestroyOnLoad, Debug, Time)
+using UnityEngine.UI; // For InputField
+using UnityEngine.SceneManagement; // For SceneManager
 
 public class DataCollector : MonoBehaviour
 {
+    // For other scripts to access DataCollector
     public static DataCollector Instance;
 
     [Tooltip("Time interval to collect Headset Position & Rotation. Default: 1.0f")]
     public float dataRecordInterval = 1f;
 
     [Header("Under Canvas")]
+    
+    // User ID InputField
     [SerializeField]
     private InputField IdInputField;
+
+    // Gender InputField - (M / m / F / f)
     [SerializeField]
     private InputField GenderInputField;
+
+    // Video ID InputField - (1 / 2 / 3 / 4)
     [SerializeField]
     private InputField VideoIdInputField;
-
-    [HideInInspector]
-    public bool startRecording = false;
+    
+    // For holding SteamVR Camera Component
     [HideInInspector]
     public GameObject user;
-    [HideInInspector]
-    public int VideoID;
+
+    // For converting User Input into respective data
+    private string dataID;
     [HideInInspector]
     public string Gender;
+    [HideInInspector]
+    public int VideoID;
     
-    private string dataID;
+    // For Recording Head Movement Data
+    private bool startRecording = false;
     private float time = 0f;
 
+    // Runs before Start()
     private void Awake()
     {
+        // Allow DataCollector Instance to be alive after change scene
         DontDestroyOnLoad(this);
+        
+        // Creates a folder to hold data output if the folder does not exist.
         if (!Directory.Exists(Application.dataPath + "/Data"))
         {
             Directory.CreateDirectory(Application.dataPath + "/Data");
         }
     }
 
+    // Runs at the start of first frame
     private void Start()
     {
+        // Set this instance of DataCollector to allow other scripts to access its variables and data
         Instance = this;
-        // Allow input
+        
+        // Allow input of User ID
         IdInputField.Select();
     }
+
+    // Runs at every frame
+    private void Update()
+    {
+        // Start Recording when bool is set to true
+        if (startRecording)
+        {
+            time += Time.deltaTime;
+
+            // Record Head Movement Data every data Record Interval(Default: 1.0f)
+            if (time > dataRecordInterval)
+            {
+                // Reset timer
+                time = 0;
+                
+                // Write generated data into csv file
+                PushData(GenerateData());
+            }
+        }
+    }
     
+    // Returns generated Head Movement Data string
+    private string GenerateData()
+    {
+        string data = "";
+        
+        // Get Time Information into data string
+        data += System.DateTime.Now.ToString("HH");
+        data += ":";
+        data += System.DateTime.Now.ToString("mm");
+        data += ":";
+        data += System.DateTime.Now.ToString("ss");
+        data += ":";
+        data += System.DateTime.Now.ToString("FFF");
+
+        // Seperator
+        data += ",";
+
+        // Get Headset Position in vector 3 format
+        string posstr = user.GetComponent<SteamVR_Camera>().head.transform.position.ToString("F3");
+
+        // Change , to . to prevent Position data to be seperated
+        data += ChangeLetters(posstr, ',', '.');
+
+        // Seperator
+        data += ",";
+
+        // Get Headset Rotation in vector 3 format
+        string rotstr = user.GetComponent<SteamVR_Camera>().head.transform.rotation.ToString("F3");
+
+        // Change , to . to prevent Position data to be seperated
+        data += ChangeLetters(rotstr, ',', '.');
+        
+        return data;
+    }
+
+    // Edit the current file by adding the new text
+    private void PushData(string text)
+    {
+        // Open csv file at the path return from GetPath()
+        StreamWriter sw = File.AppendText(GetPath());
+        
+        // Write onto the file
+        sw.WriteLine(text);
+
+        // Close the file
+        sw.Close();
+    }
+
+    // Returns the file path being used to store the data
+    private string GetPath()
+    {
+        // If the filepath already exists, create a new file with a duplicate number
+        string filePath = Application.dataPath + "/Data/" + dataID + ".csv";
+        int duplicateCounts = 0;
+        while (true)
+        {
+            if (File.Exists(filePath))
+            {
+                ++duplicateCounts;
+                filePath = Application.dataPath + "/Data/" + dataID + "(" + duplicateCounts.ToString() + ")" + ".csv";
+            }
+            else
+                break;
+        }
+        return filePath;
+    }
+
+    // Create the csv file
+    private void CreateCSV()
+    {
+        // Create csv file with the path return from GetPath()
+        StreamWriter output = File.CreateText(GetPath());
+
+        // Write Title of Data going to be recorded
+        output.WriteLine("Time, Position, Rotation");
+
+        // Close file
+        output.Close();
+    }
+
+    // Change "letter" in "str" to "toBeLetter"
+    private string ChangeLetters(string str, char letter, char toBeLetter)
+    {
+        char[] ret = str.ToCharArray();
+        for (int i = 0; i < ret.Length; ++i)
+        {
+            if (ret[i] == letter)
+            {
+                ret[i] = toBeLetter;
+            }
+        }
+        return new string(ret);
+    }
+    
+    //--------------------------------------------------------------------------------
+    //                                  PUBLIC FUNCTIONS
+    //--------------------------------------------------------------------------------
+
+    // Link to User ID InputField OnEndEdit()
+    // This Registers User ID and use it to create csv file.
+    // Then Set Gender InputField to active and allow its input.
     public void ProceedToStateGender()
     {
         // If no text, dont let them proceed
         if (IdInputField.text == null)
             return;
 
+        // Register User ID
         dataID = IdInputField.text;
 
+        // Create csv file with User ID as file name
         CreateCSV();
 
-        // Allow input
+        // Allow input of Gender
         GenderInputField.gameObject.SetActive(true);
         GenderInputField.Select();
     }
 
+    // Link to Gender InputField OnEndEdit()
+    // This Registers Gender based on input, which is used to determine the directory of video files.
+    // Then Set Video ID InputField to active and allow its input.
     public void ProceedToSelectVideo()
     {
         // If no text, dont let them proceed
@@ -84,15 +238,19 @@ public class DataCollector : MonoBehaviour
                 break;
         }
 
-        // Allow input
+        // Allow input of Video ID
         VideoIdInputField.gameObject.SetActive(true);
         VideoIdInputField.Select();
     }
 
+    // Link to Video ID InputField OnEndEdit()
+    // This Registers Video ID, which is used to determine the video file to play.
+    // Then Set bool to true so can start Recording Head Movement Data.
+    // And Change Scene to Main Scene (Watch video)
     public void Start360MovieTheatre()
     {
         // Try Parse VideoID Input into int and put in VideoID
-        if(VideoIdInputField.text == null || !int.TryParse(VideoIdInputField.text,out VideoID))
+        if (VideoIdInputField.text == null || !int.TryParse(VideoIdInputField.text, out VideoID))
         {
             // If no text, dont let them proceed
             Debug.LogWarning("VideoID Input cannot be parse into int.");
@@ -104,88 +262,5 @@ public class DataCollector : MonoBehaviour
 
         // Change to MainScene
         SceneManager.LoadScene("MainScene");
-    }
-
-    // Generate Head Movement Data
-    private string GenerateData()
-    {
-        string data = "";
-        data += System.DateTime.Now.ToString("HH");
-        data += ":";
-        data += System.DateTime.Now.ToString("mm");
-        data += ":";
-        data += System.DateTime.Now.ToString("ss");
-        data += ":";
-        data += System.DateTime.Now.ToString("FFF");
-        data += ",";
-        string posstr = user.GetComponent<SteamVR_Camera>().head.transform.position.ToString("F3");
-        data += ChangeLetters(posstr, ',', '.');
-        data += ",";
-        string rotstr = user.GetComponent<SteamVR_Camera>().head.transform.rotation.ToString("F3");
-        data += ChangeLetters(rotstr, ',', '.');
-        return data;
-    }
-
-    private void Update()
-    {
-        if (startRecording)
-        {
-            // Record Head Movement Data every dataRecordInterval
-            time += Time.deltaTime;
-            if (time > dataRecordInterval)
-            {
-                time = 0;
-                PushData(GenerateData());
-            }
-        }
-    }
-
-    // Edit the current file by adding the new text
-    private void PushData(string text)
-    {
-        StreamWriter sw = File.AppendText(GetPath() + ".csv");
-        sw.WriteLine(text);
-        sw.Close();
-    }
-
-    // Returns the file path being used to store the data
-    private string GetPath()
-    {
-        // If the filepath already exists, create a new file with a duplicate number
-        string filePath = Application.dataPath + "/Data/" + dataID;
-        int duplicateCounts = 0;
-        while (true)
-        {
-            if (Directory.Exists(filePath))
-            {
-                ++duplicateCounts;
-                filePath = Application.dataPath + "/Data/" + dataID + "(" + duplicateCounts.ToString() + ")";
-            }
-            else
-                break;
-        }
-        return filePath;
-    }
-
-    private void CreateCSV()
-    {
-        // Create the csv file
-        StreamWriter output = File.CreateText(GetPath() + ".csv");
-        output.WriteLine("Time, Position, Rotation");
-        output.Close();
-    }
-
-    // Change "letter" in "str" to "toBeLetter"
-    private string ChangeLetters(string str, char letter, char toBeLetter)
-    {
-        char[] ret = str.ToCharArray();
-        for (int i = 0; i < ret.Length; ++i)
-        {
-            if (ret[i] == letter)
-            {
-                ret[i] = toBeLetter;
-            }
-        }
-        return new string(ret);
     }
 }
--- MnM-CoinProject/Assets/Scripts/Video.cs
+++ MnM-CoinProject/Assets/Scripts/Video.cs
@@ -1,39 +1,66 @@
-﻿using System.IO;
-using UnityEngine;
-using UnityEngine.Video;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
+﻿//--------------------------------------------------------------------------------
+/*
+ * This Script is used for playing 360 Degree Video once, and the application will close afterwards.
+ * Allow Inputs to : Restart(Load StartScene and Input User ID and video choice again),
+ *                   Increase Volume,
+ *                   Decrese Volume
+ * 
+ * Used in Main Scene, attached to Empty GameObject "Video", with Video Player Component attached.
+ * Component Settings : Source(URL), URL(Leave Empty), PlayOnAwake(False),
+ *                      WaitForFirstFrame(True), Loop(False), PlayBackSpeed(1),
+ *                      RenderMode(Render Texture), TargetTexture(None), AspectRatio(Fit Horizontally),
+ *                      AudioOutputMode(Direct), ControlledTrack(1), Track 0(True),
+ *                      Mute(False), Volume(Default: 0.5)
+ */
+//--------------------------------------------------------------------------------
+
+using System.IO;    // For Directory, File, StreamWriter
+using UnityEngine;  // Default Unity Script (MonoBehaviour, Application, Tooltip, SerializeField, HideInInspector, DontDestroyOnLoad, Debug, Time)
+using UnityEngine.Video; // For VideoPlayer, VideoRenderMode, VideoSource
+using System.Collections.Generic; // For List<>
+using UnityEngine.SceneManagement; // For SceneManager
 
 public class Video : MonoBehaviour
 {
-    [HideInInspector]
-    public VideoPlayer videoPlayer;
-    [HideInInspector]
-    public string Gender;
-    [HideInInspector]
-    public int VideoIndex;
+    private VideoPlayer videoPlayer;
 
+    // Holds video files loaded from designated path
     private List<string> videoUrls;
 
-    // Applies Gender and VideoIndex if DataCollector is alive
+    // Variables to hold data from DataCollector's instance, to determine where to look for video files
+    private string Gender;
+    private int VideoIndex;
+
+    // Runs before Start()
     private void Awake()
     {
         if(DataCollector.Instance != null)
         {
-            //find the steamvr eye and assign it to data collector
+            // Find the SteamVR eye GameObject and assign it to DataCollector
             DataCollector.Instance.user = FindObjectOfType<SteamVR_Camera>().gameObject;
+
+            // Applies Gender and VideoIndex if DataCollector is alive
             Gender = DataCollector.Instance.Gender;
             VideoIndex = DataCollector.Instance.VideoID;
         }
     }
 
-    // Loads video and Plays it
+    // Runs at the start of first frame
     private void Start ()
     {
+        // Get Video Player Component
         videoPlayer = GetComponent<VideoPlayer>();
+        
+        // Intiate List
         videoUrls = new List<string>();
+
+        // Loads video based on Gender which user input in Start Scene
         LoadVideos(Gender);
+
+        // Plays video based on Video Index which user input in Start Scene
         CheckDimensions(videoUrls[VideoIndex - 1]);
+
+        // Listen for video finish playing's event call, calls FinishPlaying() afterwards
         videoPlayer.loopPointReached += FinishPlaying;
 	}
 
@@ -42,32 +69,43 @@ public class Video : MonoBehaviour
     {
 
 #if UNITY_EDITOR
+        
+        // Load videos from C drive if running through Unity Editor
         DirectoryInfo directory = new DirectoryInfo(@"C:\Videos\" + Folder + "\\");
 #else
+        // Load videos from Application Data if running through Release exe file
         DirectoryInfo directory = new DirectoryInfo(Application.dataPath + "/Videos/" + Folder + "/");
 #endif
 
+        // Loads every mp4 file path into List of string, videoUrls
         foreach (var file in directory.GetFiles("*.mp4", SearchOption.AllDirectories))
         {
             videoUrls.Add(directory.FullName + file.Name);
         }
+
+        // Trim Excess to prevent extra space in List
         videoUrls.TrimExcess();
     }
 
     // Make a temporary VideoPlayer GameObject to get dimensions of chosen video
+    // Proceed to play the video as soon as it is prepared
     private void CheckDimensions(string url)
     {
+        // Preparing information for video player component
         GameObject tempVideo = new GameObject();
         VideoPlayer tempvideoPlayer = tempVideo.AddComponent<VideoPlayer>();
         tempvideoPlayer.renderMode = VideoRenderMode.RenderTexture;
         tempvideoPlayer.targetTexture = new RenderTexture(1, 1, 0);
         tempvideoPlayer.source = VideoSource.Url;
         tempvideoPlayer.url = url;
+
+        // Listen for prepare complete event call, and proceed to play the video with prepared information
         tempvideoPlayer.prepareCompleted += (VideoPlayer source) =>
         {
             PlayVideo(url, source.texture.width, source.texture.height);
             Destroy(tempVideo);
         };
+
         tempvideoPlayer.Prepare();
     }
 
@@ -77,6 +115,8 @@ public class Video : MonoBehaviour
         videoPlayer.url = url;
         RenderTexture texture = new RenderTexture(width, height, 24);
         videoPlayer.targetTexture = texture;
+
+        // Set RenderTexture onto Skybox and Play the video on Skybox
         RenderSettings.skybox.SetTexture("_MainTex", texture);
         videoPlayer.Play();
     }
@@ -87,10 +127,11 @@ public class Video : MonoBehaviour
         // Stops playing video
         _videoPlayer.Stop();
 
-        // Quit Application
 #if UNITY_EDITOR
+        // Stop running Application
         UnityEditor.EditorApplication.isPlaying = false;
 #else
+        // Quit Application
         Application.Quit();
 #endif
     }
@@ -99,15 +140,35 @@ public class Video : MonoBehaviour
     private void Restart()
     {
         SceneManager.LoadScene("StartScene");
+        // Destroy current DataCollector Instance, as StartScene will have its new instance of DataCollector
         Destroy(DataCollector.Instance.gameObject);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // Proceed to Restart if 'R' is pressed
         if(Input.GetKeyUp(KeyCode.R))
         {
             Restart();
+        }
+
+        // Increase volume by 0.1f if 'Up Arrow' is pressed
+        if(Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            if(videoPlayer.GetDirectAudioVolume(0) + 0.1f < 1f)
+                videoPlayer.SetDirectAudioVolume(0, videoPlayer.GetDirectAudioVolume(0) + 0.1f);
+            else
+                videoPlayer.SetDirectAudioVolume(0, 1f);
+        }
+
+        // Decrease volume by 0.1f if 'Down Arrow' is pressed
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            if (videoPlayer.GetDirectAudioVolume(0) - 0.1f > 0f)
+                videoPlayer.SetDirectAudioVolume(0, videoPlayer.GetDirectAudioVolume(0) - 0.1f);
+            else
+                videoPlayer.SetDirectAudioVolume(0, 0f);
         }
     }
 }
